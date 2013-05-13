a02a262dab173659b0101402bddc9bd11b898f4b
kaestel <martin@hvadhedderde.com>
2013-05-13 15:22:58 +0200

    Added submodules

--- src/www/js/lib/seg_basic_include.js
+++ src/www/js/lib/seg_basic_include.js
@@ -2,4 +2,4 @@
 document.write('<script type="text/javascript" src="/js/jes/full/seg_basic.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-googleanalytics.js"></script>');
--- src/www/js/lib/seg_mobile_touch_include.js
+++ src/www/js/lib/seg_mobile_touch_include.js
@@ -1,9 +1,9 @@
-document.write('<script type="text/javascript" src="/js/framework/bundles/full/seg_mobile_touch.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/beta-u-video.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/full/seg_mobile_touch.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/beta-u-video.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/framework/u-init-static.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-init-static.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/i-page-mobile_touch.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-googleanalytics.js"></script>');
